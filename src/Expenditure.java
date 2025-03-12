cd24a388d52df554ecfa637819f511468f747250
Мартынова Екатерина Михайловна <EMMartynova@alfabank.ru>
2025-03-12 20:06:14 +0300

    Домашнее задание к занятию "Полиморфизм"

--- src/Expenditure.java
+++ src/Expenditure.java
@@ -1,5 +1,5 @@
 public class Expenditure extends Deal {
     public Expenditure(String title, int price) {
-        super(("Покупка" + title + "на " + price +"руб."),price, 0);
+        super(("Покупка " + title + "на " + price + " руб."),price, 0);
     }
 }
